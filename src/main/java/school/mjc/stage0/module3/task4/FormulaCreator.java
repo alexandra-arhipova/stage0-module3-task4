f7a7c6797322b168f21be95bc97f21c81ff4046e
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-11-01 14:41:28 +0200

    module 3 task 4 completed.

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 9;
+        char b = 'a';
+        int c = 5;
+        char d = 'b';
+        int e = 2;
+        int f = 7;
+        int g = 4;
+        System.out.println((a * (b * b) + (c * e) + b - f) * ( b + d - ( g * b * d)/ e));
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int letter = 'c';
+        int letter1 = 'h';
+        int letter2 = 'a';
+        int letter3 = 'r';
     }
 }
