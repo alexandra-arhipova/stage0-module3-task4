d56051b04567fd841fd35662203772c31e7f2d83
alexandra-arhipova <alexandra9.arhipova@gmail.com>
2023-11-01 20:57:40 +0200

    module 3 task 4 completed.

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,13 +2,19 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int letter = 'c';     //99
+        int letter1 = 'h';    //104
+        int letter2 = 'a';    //97
+        int letter3 = 'r';    //114
+
         char c = 99;
         char h = 104;
         char a = 97;
         char r = 114;
-        System.out.print(c);
-        System.out.print(h);
-        System.out.print(a);
-        System.out.print(r);
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
     }
 }
